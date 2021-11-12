4d4974d1a3ba7b5833a5417b143a04a0b4b1e969
KimEunho <82789634+uno-km@users.noreply.github.com>
2021-11-12 21:14:38 +0900

    CRUD기능을 위해 자료구조 일부수정

--- SemiProjects/DataBase/Table/Table.java
+++ SemiProjects/DataBase/Table/Table.java
@@ -1,12 +1,15 @@
 package Table;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
+
 import UnoLibrary.Unotools;
 
 public class Table {
 	private String tableName = "";
 	private ArrayList<String> columm = new ArrayList<String>();
 	private ArrayList<ArrayList<String>> recRow = new ArrayList<ArrayList<String>>();
+	private ArrayList<LinkedHashMap<ArrayList<String>, String>> recRowTest = new ArrayList<LinkedHashMap<ArrayList<String>, String>>();
 
 	public String getTableName() {
 		return tableName;
@@ -38,5 +41,16 @@ public class Table {
 
 	public void setRecRow(ArrayList<ArrayList<String>> recRow) {
 		this.recRow = recRow;
+	}
+
+	/*
+	 * 21.11.12 CRUD 기능을 구현하기위해 자료구조를 일부 수정이 필요하여 추가함
+	 */
+	public ArrayList<LinkedHashMap<ArrayList<String>, String>> getRecRowTest() {
+		return recRowTest;
+	}
+
+	public void setRecRowTest(ArrayList<LinkedHashMap<ArrayList<String>, String>> recRowTest) {
+		this.recRowTest = recRowTest;
 	}
 }
